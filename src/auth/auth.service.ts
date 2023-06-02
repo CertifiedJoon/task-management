dc9a9de2191fea92cb7cc868be0bd97f10c6d55f
Joon_Thinkpad <mjypark1212@gmail.com>
2023-06-02 17:53:08 +0800

    Add: SignIn Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+    const user = this.usersRepository.create({ username, password });
+    await this.usersRepository.save(user);
+    return user;
+  }
+}
