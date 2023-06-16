cd88808fc1a01666bcbf2381b20c8fede95fa769
Joon_Thinkpad <mjypark1212@gmail.com>
2023-06-16 17:49:44 +0800

    Add: Iterable

---
+++ Javascript/DataTypes/iterable.js
@@ -0,0 +1,71 @@
+let range = {
+  from: 1,
+  to: 5,
+};
+
+// 1. call to for..of initially calls this
+range[Symbol.iterator] = function () {
+  // ...it returns the iterator object:
+  // 2. Onward, for..of works only with the iterator object below, asking it for next values
+  return {
+    current: this.from,
+    last: this.to,
+
+    // 3. next() is called on each iteration by the for..of loop
+    next() {
+      // 4. it should return the value as an object {done:.., value :...}
+      if (this.current <= this.last) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    },
+  };
+};
+
+// now it works!
+for (let num of range) {
+  console.log(num); // 1, then 2, 3, 4, 5
+}
+
+for (let char of 'test') {
+  // triggers 4 times: once for each character
+  console.log(char); // t, then e, then s, then t
+}
+
+let str = 'Hello';
+
+// does the same as
+// for (let char of str) console.log(char);
+
+let iterator = str[Symbol.iterator]();
+
+while (true) {
+  let result = iterator.next();
+  if (result.done) break;
+  console.log(result.value); // outputs characters one by one
+}
+
+// Array.from()
+
+let arrayLike = {
+  0: 'Hello',
+  1: 'World',
+  length: 2,
+};
+
+let arr = Array.from(arrayLike);
+console.log(arr);
+
+arr = Array.from(range);
+console.log(arr);
+
+arr = Array.from(range, (num) => num * num);
+console.log(arr);
+
+// array from str
+
+let chars = Array.from(str);
+for (let char of chars) {
+  console.log(char);
+}
